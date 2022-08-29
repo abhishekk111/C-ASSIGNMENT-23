d8c6e7dfb831ae3cc70e60a1ad72486a7c60d0a8
abhishekk111 <109384287+abhishekk111@users.noreply.github.com>
2022-08-29 13:04:43 +0530

    Create Q7.cpp

---
+++ Q7.cpp
@@ -0,0 +1,12 @@
+//Write a C++ program to calculate the square of a number
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x,y;
+    std::cout<<"Enter a number:";
+    std::cin>>x;
+    y=x*x;
+    std::cout<<"Square of"<<x<<"is"<<y;
+    return 0;
+}
