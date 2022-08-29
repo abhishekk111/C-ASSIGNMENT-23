63570d388e956e76773f791a90725d05728d6d79
abhishekk111 <109384287+abhishekk111@users.noreply.github.com>
2022-08-29 13:00:08 +0530

    Create Q3.cpp

---
+++ Q3.cpp
@@ -0,0 +1,14 @@
+// Write a C++ program to calculate the sum of two numbers.
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x,y;
+    std::cout<<"Enter first number:";
+    std::cin>>x;
+    std::cout<<"Enter second number:";
+    std::cin>>y;
+    std::cout<<"sum of" << x << "and" << y << "is" << x+y;
+    return 0;
+}
+
